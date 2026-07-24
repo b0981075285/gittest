4274e2b7ebc34b527a4b3f862f0405be9e2748ac
12 <a0981075285@gmail.com>
2026-07-24 11:27:29 +0800

    24

--- test.py
+++ test.py
@@ -3,15 +3,15 @@ import random
 
 WELCOME_MESSAGE = "猜數字"
 
-MAX_ATTEMPTS = 3
+MAX_ATTEMPTS = 9
 
 
 
 def play_game():
-    answer = random.randint(1, 20)
+    answer = random.randint(1, 100)
 
     print(WELCOME_MESSAGE)
-    print(" 1 到 20 。")
+    print(" 1 到 100 。")
 
     for attempt in range(1, MAX_ATTEMPTS + 1):
         guess = int(input(f"第 {attempt} 次猜測："))
