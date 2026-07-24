d613e6c1064a55e376cba8aae40764a47f46260c
Dark-Gary-Kami <as0977267780@gmail.com>
2026-07-24 15:18:49 +0800

    <神>

--- test.py
+++ test.py
@@ -3,15 +3,16 @@ import random
 
 WELCOME_MESSAGE = "我不喜歡猜數字"
 
-MAX_ATTEMPTS = 3
+MAX_ATTEMPTS = 9
+#114514
 
 
 
 def play_game():
-    answer = random.randint(1, 20)
+    answer = random.randint(1, 1000)
 
     print(WELCOME_MESSAGE)
-    print(" 1 到 20 。")
+    print(" 1 到 1000 。")
 
     for attempt in range(1, MAX_ATTEMPTS + 1):
         guess = int(input(f"第 {attempt} 次猜測："))
